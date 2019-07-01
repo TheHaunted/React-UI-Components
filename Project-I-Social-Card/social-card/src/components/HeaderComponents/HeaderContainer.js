f17dd305ccc314c1bce92978263fa201ba320aa2
Eric Cabrera <Ecabrera@Erics-MacBook-Pro.local>
2019-06-30 22:13:09 -0500

    linked HeaderContainer.js to App.js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContainer = () => {
+    return (
+        <h1 className="header">Header</h1>
+    );
+};
+
+export default HeaderContainer;
